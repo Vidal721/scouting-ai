5f4e0d6a10e9a1d5229253dde2ed811537a228ba
Vidal721 <aprich1109@gmail.com>
2026-03-08 20:13:12 -0500

    UI Change

---
+++ index.js
@@ -0,0 +1,20 @@
+   // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDaPeJYCuRdy2cRHrQ7-PPgHohqTVR4CuY",
+    authDomain: "anylize-515c6.firebaseapp.com",
+    projectId: "anylize-515c6",
+    storageBucket: "anylize-515c6.firebasestorage.app",
+    messagingSenderId: "554641127477",
+    appId: "1:554641127477:web:f303d576b4510f1fa9197c",
+    measurementId: "G-SPCJL2C5FQ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
